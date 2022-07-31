38f8094cebd2d00ea426d6a30a27c6f25d133f64
Yojacni <57448005+yojacni@users.noreply.github.com>
2022-07-31 18:14:45 -0500

    send iOS sms test2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import UIKit
 import Flutter
+import UIKit
+import MessageUI
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,12 +8,56 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
     guard let viewController = window?.rootViewController as? FlutterViewController else {
           fatalError("Invalid view controller")
     }
-    let platformChannelController = PlatformChannelController(viewController: viewController)
-    platformChannelController.setupChannel()
-    GeneratedPluginRegistrant.register(with: self)
+    let platformChannelController = FlutterMethodChannel(name: "com.example.platform_channels_example/sendsms", binaryMessenger: viewController as! FlutterBinaryMessenger)
+    
+    platformChannelController.setMethodCallHandler{ (call, result) in
+    switch call.method {
+    case "utils:send-sms":
+      let string = "Este es un texto de ejemplo"
+        if (MFMessageComposeViewController.canSendText()) {
+            self.result?("Success")
+            let controller = MFMessageComposeViewController()
+            controller.body = string.htmlToString
+            controller.recipients = recipent
+            controller.messageComposeDelegate = self
+            UIApplication.shared.keyWindow?.rootViewController?.present(controller, animated: true, completion: nil)
+        } else {
+            self.result?("Message service is not available")
+        }
+      break;
+    default:
+      break;
+    }
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+//MARK: MFMessageComposeViewControllerDelegate
+extension SwiftFlutterSocialContentSharePlugin:MFMessageComposeViewControllerDelegate{
+    
+    public func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        let map: [MessageComposeResult: String] = [
+            MessageComposeResult.sent: "sent",
+            MessageComposeResult.cancelled: "cancelled",
+            MessageComposeResult.failed: "failed",
+        ]
+        if let callback = self.result {
+            callback(map[result])
+        }
+        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+//MARK: MFMailComposeViewControllerDelegate
+extension SwiftFlutterSocialContentSharePlugin: MFMailComposeViewControllerDelegate{
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
 }
--- ios/Runner/PlatformChannelController.swift
+++ ios/Runner/PlatformChannelController.swift
@@ -1,46 +1,44 @@
-//
-//  PlatformChannelController.swift
-//  Runner
-//
-//  Created by user198486 on 7/24/22.
-//
+// //
+// //  PlatformChannelController.swift
+// //  Runner
+// //
+// //  Created by user198486 on 7/24/22.
+// //
 
-import Foundation
+// import Foundation
 
-class PlatformChannelController{
-  static let channelName = "com.example.platform_channels_example/sendsms"
+// class PlatformChannelController{
+//   static let channelName = "com.example.platform_channels_example/sendsms"
 
-  unowned let viewController: FlutterViewController
-  let channel: FlutterMethodChannel
+//   unowned let viewController: FlutterViewController
+//   let channel: FlutterMethodChannel
 
-  init(viewController: FlutterViewController){
-    self.viewController = viewController
-    self.channel = FlutterMethodChannel(name: PlatformChannelController.channelName, binaryMessenger:
-    viewController as! FlutterBinaryMessenger
-    )
-  }
+//   init(viewController: FlutterViewController){
+//     self.viewController = viewController
+//     self.channel = FlutterMethodChannel(name: PlatformChannelController.channelName, binaryMessenger:
+//     viewController as! FlutterBinaryMessenger
+//     )
+//   }
   
-  func setupChannel () {
-    channel.setMethodCallHandler{ (call, result) in
-    switch call.method {
-    case "utils:send-sms":
-      updateUI()
-      // let alert = UIAlertController(title: "Mensaje", message: "Hola", preferredStyle: .alert)
-      // alert.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
-      // self.viewController.present(alert, animated: true, completion: nil)
-      break;
-    default:
-      break;
-    }
-    }
-  }
-
-  func updateUI(){
-        messageView.layer.shadowColor = UIColor.black.cgColor
-        messageView.layer.shadowOpacity = 0.2
-        messageView.layer.shadowOffset = .zero
-        messageView.layer.shadowRadius = 10
-        self.inviteMessage.text = "Hi, @ Check out Atlantic City App, Get it for free at \(inviteLink)"
-        self.inviteMessage.becomeFirstResponder()   
-    }
-}
+//   func setupChannel () {
+//     channel.setMethodCallHandler{ (call, result) in
+//     switch call.method {
+//     case "utils:send-sms":
+//       // let alert = UIAlertController(title: "Mensaje", message: "Hola", preferredStyle: .alert)
+//       // alert.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
+//       // self.viewController.present(alert, animated: true, completion: nil)
+//      if MFMessageComposeViewController.canSendText() == true{
+//                 let recipients:[String] = ["+5358193403"]
+//                 let messageController = MFMessageComposeViewController()
+//                 messageController.messageComposeDelegate  = self // implement delegate if you want
+//                 messageController.recipients = recipients
+//                 messageController.body = "Esto es un mensaje de prueba"
+//                 self.viewController.present(messageController, animated: true, completion: nil)
+//             }
+//       break;
+//     default:
+//       break;
+//     }
+//     }
+//   }
+// }
